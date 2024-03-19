09808d385a856bc6dae2d0633021282be8ab5342
Andrew Entwistle <aentwistle@theorchard.com>
2024-03-19 15:18:19 +0000

    Updated to automatically get PR when icon clicked

--- button.js
+++ button.js
@@ -2,14 +2,24 @@ const buttonId = 'copyButton';
 const unsupportedAlert = 'Unsupported page, only use this extension on github PR pages';
 
 document.getElementById(buttonId).onclick = async () => {
+  await copyPrDetailsToClipboard();
+};
+
+window.onload = async () => {
+  await copyPrDetailsToClipboard();
+};
+
+async function copyPrDetailsToClipboard() {
   const queryInfo = {
     active: true,
     currentWindow: true,
   };
   const [tab] = await chrome.tabs.query(queryInfo);
   const url = tab.url;
-  if (!checkSupported(url))
-    return
+  if (!checkSupported(url)) {
+    window.close();
+    return;
+  }
   const text = await chrome.scripting.executeScript({
     target: {tabId: tab.id},
     func: () => document.getElementsByClassName(
@@ -18,19 +28,18 @@ document.getElementById(buttonId).onclick = async () => {
   const prName = text[0].result;
   await navigator.clipboard.writeText(url + ' - ' + prName);
   const originalText = document.getElementById(buttonId).innerHTML;
-  document.getElementById(buttonId).innerHTML = 'PR copied to clipboard';
+  document.getElementById(buttonId).innerHTML = 'PR Copied to Clipboard';
   setTimeout(() => {
     document.getElementById(buttonId).innerHTML = originalText;
   }, 5000);
-
-};
+}
 
 function checkSupported(url) {
   const validRegex = /https:\/\/github.com\/.*\/pull\/\d+/;
-  console.log(url)
+  console.log(url);
   if (!validRegex.test(url)) {
     alert(unsupportedAlert);
-    return false
+    return false;
   }
-  return true
+  return true;
 }
