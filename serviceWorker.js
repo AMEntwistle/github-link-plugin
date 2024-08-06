bde2e36ca933c476b3fc287daa6f33521412d9a3
AMEntwistle <aentwistle@sonymusic-pde.com>
2024-08-06 12:15:05 +0100

    Retrigger updated message to avoid jenkins actions removing title clickabilitiy

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,19 +1,29 @@
-async function handleUpdated(tabId, changeInfo, tabInfo) {
-  if (changeInfo.status === 'complete') {
-    const queryInfo = {
-      active: true,
-      currentWindow: true,
-    };
-    const [tab] = await chrome.tabs.query(queryInfo);
-    const validRegex = /https:\/\/github.com\/.*\/pull\/\d+/;
-    if (tab.url && validRegex.test(tab.url)) {
-      chrome.tabs.sendMessage(tabId, {action: 'tabUpdated'}).catch((e) => {
+function sendUpdated(tabId) {
+    chrome.tabs.sendMessage(tabId, {action: 'tabUpdated'}).catch((e) => {
         if (e.message !== 'Could not establish connection.' +
             ' Receiving end does not exist.')
-          throw e;
-      });
-    }
-  }
+            throw e;
+    })
 }
 
-chrome.tabs.onUpdated.addListener((handleUpdated));
+async function handleUpdated(tabId, changeInfo, tabInfo) {
+    if (changeInfo.status === 'complete') {
+        const queryInfo = {
+            active: true,
+            currentWindow: true,
+        };
+        const [tab] = await chrome.tabs.query(queryInfo);
+        const validRegex = /https:\/\/github.com\/.*\/pull\/\d+/;
+        if (tab.url && validRegex.test(tab.url)) {
+            sendUpdated(tabId)
+            setTimeout(() => {
+                sendUpdated(tabId)
+            }, 2000)
+            setTimeout(() => {
+                sendUpdated(tabId)
+            }, 4000)
+        }
+    }
+}
+
+chrome.tabs.onUpdated.addListener((handleUpdated))
